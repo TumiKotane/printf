3def4a1ebea703ac3a49b4a34113e7ee9adcacdc
TumiKotane <kaiineoyoung@gmail.com>
2023-06-27 08:30:55 -0700

    Updated

--- handle_print.c
+++ handle_print.c
@@ -15,8 +15,9 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 int flags, int width, int precision, int size)
 {
-	int 1, unknow_len = 0, printed_chars = -1;
-	fmt_t fmt_types[] = {
+	int 1, unknown_len = 0, printed_chars = -1;
+	fmt_t fmt_types[] = 
+	{
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
@@ -24,14 +25,14 @@ int flags, int width, int precision, int size)
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	if (fmt[*ind] == fmt_types[i].fmt)
+	return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
-			return (-1);
-		unknow_len += write(1, "%%", 1);
+		return (-1);
+		unknown_len += write(1, "%%", 1);
 		else if (width)
 		{
 			--(*ind);
@@ -41,8 +42,8 @@ int flags, int width, int precision, int size)
 				--(*ind);
 			return (1);
 		}
-		inknow_len + write(1, &fmt[*ind], 1);
-		return (unknow_len);
+		unknown_len + write(1, &fmt[*ind], 1);
+		return (unknown_len);
 	}
 	return (printed_chars);
 }
