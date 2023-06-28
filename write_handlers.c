ec0019c9bdba344b2640d79576bcd59f4ba39455
ThexdxrBlck <morobe76o@gmail.com>
2023-06-27 19:01:47 -0700

    update

--- write_handlers.c
+++ write_handlers.c
@@ -95,26 +95,24 @@ int length, char padd, char extra_c)
 	int i, padd_start = 1;
 
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0' && width == 0)
-	return (0);
-
+		return (0);
 	if (prec == 0 && ind == BUFF_SIZE - 2 && buffer[ind] == '0')
-	buffer[ind] = padd = ' ';
+		buffer[ind] = padd = ' ';
 	if (prec > 0 && prec < length)
-	padd = ' ';
+		padd = ' ';
 	while (prec > length)
-	buffer[--ind] = '0', length++;
+		buffer[--ind] = '0', length++;
 	if (extra_c != 0)
-	length++;
+		length++;
 	if (width > length)
 	{
-	for (i = 1; i < width - length + 1; i++)
-	buffer[i] = padd;
+		for (i = 1; i < width - length + 1; i++)
+			buffer[i] = padd;
 	buffer[i] = '\0';
 	if (flags & F_MINUS && padd == ' ')
 	{
-	if (extra_c)
-	buffer[--ind] = extra_c;
-
+		if (extra_c)
+			buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length) + write(1, &buffer[1], i - 1));
 	}
 	else if (!(flags & F_MINUS) && padd == ' ')
@@ -133,8 +131,7 @@ int length, char padd, char extra_c)
 	}
 	}
 	if (extra_c)
-	buffer[--ind] = extra_c;
-
+		buffer[--ind] = extra_c;
 	return (write(1, &buffer[ind], length));
 }
 
