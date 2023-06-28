d7675cd2c9f99a742fda64750f144018bf24b619
TumiKotane <kaiineoyoung@gmail.com>
2023-06-27 17:51:59 -0700

    Updated

--- handle_print.c
+++ handle_print.c
@@ -16,13 +16,9 @@
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-<<<<<<< HEAD
-	int i, unknow_len = 0, printed_chars = -1;
-=======
 	int i;
 	int unknown_len = 0;
 	int printed_chars = -1;
->>>>>>> refs/remotes/origin/master
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -30,29 +26,22 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-<<<<<<< HEAD
+
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+	{
 		if (fmt[*ind] == fmt_types[i].fmt)
 			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
-=======
->>>>>>> refs/remotes/origin/master
-
+	}
 	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
-<<<<<<< HEAD
-		unknow_len += write(1, "%%", 1);
-		if (fmt[*ind - 1] == ' ')
-			unknow_len += write(1, " ", 1);
-=======
 
 		unknown_len += write(1, "%%", 1);
 
 		if (fmt[*ind - 1] == ' ')
 			unknown_len += write(1, " ", 1);
 
->>>>>>> refs/remotes/origin/master
 		else if (width)
 		{
 			--(*ind);
@@ -62,13 +51,8 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 				--(*ind);
 			return (1);
 		}
-<<<<<<< HEAD
-		unknow_len += write(1, &fmt[*ind], 1);
-		return (unknow_len);
-=======
 		unknown_len += write(1, &fmt[*ind], 1);
 		return (unknown_len);
->>>>>>> refs/remotes/origin/master
 	}
 	return (printed_chars);
 }
